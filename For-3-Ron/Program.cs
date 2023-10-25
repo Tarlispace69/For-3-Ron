72876157cec38635d592a71bbf9fe4864adf05d7
Tarlispace69 <116681079+Tarlispace69@users.noreply.github.com>
2023-10-25 13:09:37 +0300

    For-3-Ron Complete

---
+++ For-3-Ron/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace For_3_Ron
+{
+    class Program
+    {
+        const int LIMIT = 300;
+        static void Main(string[] args)
+        {
+            int i = 0, amount = 0;
+            for (i=0; i< LIMIT; i++)
+            {
+                if (Math.Sqrt(i) == (int)Math.Sqrt(i))
+                {
+                    amount++;
+                }
+            }
+            Console.WriteLine($"Amount of integer squares is: {amount}");
+            Console.ReadLine();
+        }
+    }
+}
